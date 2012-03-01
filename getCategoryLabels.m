de9b5a2cef85e2457c38c44b4aeae2bddacb4aa9
unknown <Brent@Brent-PC.(none)>
2012-03-01 10:58:41 -0800

    update

--- getCategoryLabels.m
+++ getCategoryLabels.m
@@ -1,4 +1,4 @@
-function imageSequenceCategories = getCategoryLabels(imageSequence, TYPES)
+function imageSequenceCategories = getCategoryLabels(imageSequenceTarget, TYPES)
 
 % function imageSequenceCategories = getCategoryLabels(imageSequence, TYPES)
 % returns category ids for each image in the image sequence
@@ -7,9 +7,9 @@ imageTypeNames = {'b','a1','a2','a3','c1','c2','c3','c4','d','z1','z2','z3','t'}
 lowerbounds = [(0:100:800) (1100:100:1300) (9000)]';
 upperbounds = lowerbounds + 101;
 
-for trial = 1:length(imageSequence)
+for trial = 1:length(imageSequenceTarget)
 
-    imageNumber = imageSequence(trial);
+    imageNumber = imageSequenceTarget(trial);
     imageType = imageTypeNames{(imageNumber > lowerbounds) & (imageNumber < upperbounds)};
     image = rem(imageNumber, 100);
 
